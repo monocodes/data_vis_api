7ffe83de0e2d3b05e331243578cfc04d0161abe7
serjCookies <locobu@icloud.com>
2022-11-28 18:57:04 +0400

    Add notes.sh file
    notes.sh for NOTEs from the book

---
+++ notes.sh
@@ -0,0 +1,5 @@
+### Monitoring API Rate Limits ------------------
+### NOTE ###
+"""
+Many APIs require you to register and obtain an API key to make API calls. As of this writing, GitHub has no such requirement, but if you obtain an API key, your limits will be much higher.
+"""
